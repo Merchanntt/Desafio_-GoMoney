1d7a305e1ed43d1fdf8e8e647f425a4cb72d2221
Adolfo Eduardo <dolfoeduardo2@gmail.com>
2020-04-23 23:57:11 -0300

    Transaçoes prontas

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,38 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions
+      .filter(transactions => {
+        return transactions.type === 'income';
+      })
+      .reduce((incomeOperetion, transactions) => {
+        return transactions.value + incomeOperetion;
+      }, 0);
+    const outcome = this.transactions
+      .filter(transactions => {
+        return transactions.type === 'outcome';
+      })
+      .reduce((outcomeOperation, transactions) => {
+        return transactions.value + outcomeOperation;
+      }, 0);
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Omit<Transaction, 'id'>): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
